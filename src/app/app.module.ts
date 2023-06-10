3dc473383ce4b533b853005714c40603b502d622
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-06-10 02:44:04 -0300

    task components, task interface, task mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
 import { AddTaskButtonComponent } from './componets/add-task-button/add-task-button.component';
+import { TasksComponent } from './componets/tasks/tasks.component';
+import { TaskItemComponent } from './componets/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AddTaskButtonComponent
+    AddTaskButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
--- src/app/componets/add-task-button/add-task-button.component.ts
+++ src/app/componets/add-task-button/add-task-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-add-task-button',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task-button.component.css']
 })
 export class AddTaskButtonComponent implements OnInit {
+  @Input() text: string = '';
+  @Input() color: string = '';
+  @Output() btnClick = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    this.btnClick.emit();
   }
 
 }
